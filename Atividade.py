0c5cfe06bf29af91834636499875b00441271554
aninha123-png <caemilletec.2025@gmail.com>
2026-03-26 16:35:00 -0300

    enviando atv

--- Atividade.py
+++ Atividade.py
@@ -1,16 +1,10 @@
-idade = int(input("Digite sua idade:"))
-
-if idade >= 18:
-    print("Entrada permitida no evento.")
-
-elif idade >= 16:
-    aut = (input("Você possui a autorização dos responsáveis?"))
-   
-    if aut == "sim":
-        print("Entrada permitida apenas com a autorização dos responsáveis.")
-    
-    else:
-        print("Entrada não permitida no evento.")
-
-elif idade < 16:
-    print("Entrada não permitida no evento.")
+idade=int(input("Digite idade do aluno: "))
+autorizacao= input("Possui autorização dos responsáveis? (sim ou não): ")
+if idade>=18:
+    print("Entrada autorizada")
+elif idade>=16 and autorizacao== "sim":
+    print("Entrada permitida")
+elif idade < 16 and autorizacao=="sim":
+    print("Entrada permitida apenas com responsável")
+else:
+    print("Entrada não permitida no evento")
