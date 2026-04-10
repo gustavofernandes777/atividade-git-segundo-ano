0f18b9d6972818e217ae9c90a7f9d43907c2ece0
antoniovtec2025 <antoniov.tec2025@gmail.com>
2026-04-09 21:36:07 -0300

    Create atividade4.py

---
+++ Antônio Victor/atividade4.py
@@ -0,0 +1,30 @@
+print("---Menu de jogo---")
+invent=[]
+a = 0
+while a != 4:
+    print("1- Adicionar item")
+    print("2- Remover item")
+    print("3- Mostrar inventário")
+    print("4- Sair")
+    a = int(input())
+    if(a>4) or (a<1):
+        print("Número invalido! Tente novamente!")
+    elif (a == 1):
+        nome =(input("Digite o nome do item:"))
+        quant = int(input("Digite a quantidade do item:"))
+        item = (nome, quant)
+        invent.append (item)
+    elif(a == 2):
+        delete = (input("Digite o item que queira remover:"))
+        encontrado = False
+        for i in invent:
+            if i[0] == delete:
+                invent.remove (i)
+                encontrado = True
+                break
+        if not encontrado:
+            print("Item não encontrado!")
+    elif(a == 3):
+        print("---Inventário---")
+        for i in invent:
+            print("Item:", i[0], "Quantidade:", i[1])
