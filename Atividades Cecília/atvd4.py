270df6eaed102da3b7c04819afa695eb1b7b963e
Cecilianaiane <cecilia.naiane.tec2025@gmail.com>
2026-04-16 16:28:14 -0300

    Update atvd4.py

--- Atividades Cecília/atvd4.py
+++ Atividades Cecília/atvd4.py
@@ -4,7 +4,8 @@ while True:
     print("/n 1- Adicionar item")
     print("2-Remover item")
     print("3-Mostrar inventário")
-    print("4-Sair")
+    print("4- Alterar nome do item")
+    print("5-Sair")
     
     opcão= input("Escolha: ")
 
@@ -22,5 +23,13 @@ while True:
     elif opcão=="3":
         for item in inv:
             print(item)
+    
     elif opcão=="4":
+        nome=input("Nome para alterar: ")
+        for item in inv:
+            quantidade= int(input("Quantidade do intem: "))
+            if item [0]==nome:
+                inv[i]=(item,quantidade)                                                            
+
+    elif opcão=="5":
         break
