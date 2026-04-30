d7d164872895a6540dac8d15ad3267bf4193b4b5
BetoEstudos <josuesantos987.tec@gmail.com>
2026-04-30 16:48:52 -0300

    Update atividade 6.py

--- josue/atividade 6.py
+++ josue/atividade 6.py
@@ -1,25 +1,44 @@
 matriz = [
-    (1, "Palmeiras", 29, 12, 9, 2, 1, 12, "VVVEV"),
-    (2, "Flamengo", 23, 11, 7, 2, 2, 10, "VEVVV"),
-    (3, "Fluminense", 23, 12, 7, 2, 3, 6, "VVEVD"),
-    (4, "São Paulo", 20, 12, 6, 2, 4, 5, "VEDED"),
-    (5, "Bahia", 20, 11, 6, 2, 3, 3, "VVDED"),
-    (6, "Athletico-PR", 19, 12, 6, 1, 5, 3, "VDEVD"),
-    (7, "Coritiba", 19, 12, 5, 4, 3, 3, "EEVVD"),
-    (8, "Bragantino", 17, 12, 5, 2, 5, 1, "VDEDD"),
-    (9, "Botafogo", 16, 11, 5, 1, 5, 0, "VDDED"),
-    (10, "Vasco", 15, 11, 4, 3, 4, 0, "EVDDE"),
-    (11, "Vitória", 14, 11, 4, 2, 5, -2, "DEDVD"),
-    (12, "Atlético-MG", 13, 11, 3, 4, 4, -1, "EEDVD"),
-    (13, "Grêmio", 12, 11, 3, 3, 5, -3, "DEDDE"),
-    (14, "Internacional", 11, 11, 3, 2, 6, -4, "DDVED"),
-    (15, "Santos", 10, 11, 2, 4, 5, -5, "EDEDD"),
-    (16, "Cruzeiro", 9, 11, 2, 3, 6, -6, "DDDEE"),
-    (17, "Corinthians", 8, 11, 2, 2, 7, -7, "DDDDE"),
-    (18, "Mirassol", 7, 11, 2, 1, 8, -8, "DDDDD"),
-    (19, "Remo", 6, 11, 1, 3, 7, -9, "DEDDD"),
-    (20, "Chapecoense", 5, 11, 1, 2, 8, -10, "DDDED")
+    ["Palmeiras", 29, 12, 9, 2, 1, ["V", "V", "V", "E", "V"]],
+    ["Flamengo", 23, 11, 7, 2, 2, ["E", "D", "V", "V", "V"]],
+    ["Fluminense", 23, 12, 7, 2, 3, ["V", "V", "E", "D", "V"]],
+    ["São Paulo", 20, 12, 6, 2, 4, ["V", "D", "V", "V", "E"]],
+    ["Bahia", 20, 11, 6, 2, 3, ["V", "V", "D", "E", "V"]],
+    ["Athletico-PR", 19, 12, 6, 1, 5, ["D", "V", "V", "D", "V"]],
+    ["Coritiba", 19, 12, 5, 4, 3, ["E", "E", "V", "V", "D"]],
+    ["RB Bragantino", 17, 12, 5, 2, 5, ["D", "V", "D", "V", "E"]],
+    ["Botafogo", 16, 11, 5, 1, 5, ["V", "D", "V", "D", "D"]],
+    ["Vasco", 16, 12, 4, 4, 4, ["E", "V", "D", "E", "V"]],
+    ["Vitória", 15, 11, 4, 3, 4, ["D", "V", "E", "V", "D"]],
+    ["Atlético-MG", 14, 12, 4, 2, 6, ["V", "D", "D", "V", "D"]],
+    ["Grêmio", 13, 12, 3, 4, 5, ["E", "E", "V", "D", "E"]],
+    ["Internacional", 13, 12, 3, 4, 5, ["D", "V", "D", "E", "V"]],
+    ["Santos", 13, 12, 3, 4, 5, ["V", "D", "E", "D", "V"]],
+    ["Cruzeiro", 13, 12, 3, 4, 5, ["D", "E", "V", "D", "D"]],
+    ["Corinthians", 12, 12, 2, 6, 4, ["E", "D", "D", "E", "E"]],
+    ["Mirassol", 9, 11, 2, 3, 6, ["D", "D", "V", "D", "E"]],
+    ["Remo", 8, 12, 1, 5, 6, ["E", "E", "D", "D", "E"]],
+    ["Chapecoense", 8, 11, 1, 5, 5, ["D", "E", "D", "E", "D"]]
 ]
 
+matriz.append(["Barcelona", 70, 30, 22, 5, 8, ["V", "V", "V", "V", "E"]])
+matriz.remove(matriz[2])
+
 for time in matriz:
-    print(time)
+    if time[0] == "Corinthians":
+        time[1] = 150
+    elif time[0] == "Palmeiras":
+        time[1] = 19
+    elif time[0] == "Flamengo":
+        time[1] = 13
+    elif time[0] == "São Paulo":
+        time[1] = 10
+    elif time[0] == "Santos":
+        time[1] = 3
+
+matriz_ordenada = sorted(matriz, key=lambda row: row[1], reverse=True)
+
+print(matriz_ordenada[0])
+
+for posicao, time in enumerate(matriz_ordenada):
+    print(f"{posicao+1}º {time}")
