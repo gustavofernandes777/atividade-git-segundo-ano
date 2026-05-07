5d1e4a1407f2c0c07a47e16857d78435e0790263
nadjla <nadjla.tec2025@gmail.com>
2026-05-07 16:00:35 -0300

    Create ativ06.py

---
+++ Nadjla/ativ06.py
@@ -0,0 +1,28 @@
+brasileirao_2026 = [ 
+["classificacao", "P", "J", "V", "E", "D", "ultimos jogos"],
+
+["[palmeiras" , 29, 12, 9, 2, 1, ["v" , "v" , "v" , "e", "v"]],
+["flamengo", 23, 11, 7, 2, 2, ["E", "D" , "V" , "V", "V"]],
+["fluminense", 23, 12, 7 , 2 , 3, ["V" , "V" , "E" , "D" , "V"]],
+["são paulo" , 20 , 12, 6, 2, 4, ["D" , "E" , "V" , "D" , "D"]],
+["Bahia" , 20 , 11 , 6 , 2 , 3, ["D" , "V" , "D" , "V" "D"]],
+["atletico-PR" , 19 , 12 ,6 , 1 ,5, ["V" , "D" , "D" , "V" , "D"]],
+["curitiba" , 19, 12, 5, 4, 3, ["D" , "E" , "E" , "E" , "V"]],
+["bragantino" , 17, 12, 5, 2, 5, ["D" , "V" , "V", "D", "V"]],
+["botafogo" , 16, 11, 5 , 1, 5, ["V" , "V" , "V", "E" , "V"]],
+["vasco" , 16, 12 ,4, 4, 4 ,4, ["V" , "E" , "D" , "E" , "V"]],
+["votoria" , 15 , 11 , 4 , 3 ,4, ["v" , "D" , "E" , "V" , "E"]],
+["Atletico-MG" , 14, 12, 4, 2, 6, ["D" , "V" , "V" , "D" , "D"]],
+["Gremio" , 13, 12, 3, 4, 5, ["D" , "D" , "E" , "E" ,"D"]],
+["internacional" , 13, 12, 3, 4, 5, ["V" , "E" ,"V" , "E" , "D"]]          
+["Santos" , 13, 12, 34, 5, ["E" , "V" , "D" , "V" , "D"]],
+["Cruzerio" , 13, 12, 34, 5, ["E" , "V" , "D" , "V" , "V"]],
+["Corinthians" , 12, 12 ,2 , 6, 4, ["E" , "D" , "D" , "E" , "E"]],
+["Mirassol" , 9, 11 , 2, 3, 6, ["D", "D" , "D" , "D" ,"V"]], 
+["Remo" ,8, 12, 1, 5, 6, ["V" , "D" , "E" ,"E" , "D"]],
+["Chapecoense" ,8, 11, 1, 5, 5, ["D" ,"D" , "E" , "D" ,"D"]]
+
+]
+ 
+for linha in brasileirao_2026:
+        print(linha)
