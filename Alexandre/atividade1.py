1d94fbda185ed5906baa1f391d968b8617788faa
alexandredesenvolvedor <cortenevesalexandre@gmail.com>
2026-05-28 18:35:39 -0300

    Add files via upload

--- Alexandre/atividade1.py
+++ Alexandre/atividade1.py
@@ -1,18 +1,16 @@
-qt_alunos = int(input("digite a quantidade de alunos: "))
-qt_nota = int (input("digite a qauntidade de notas: "))
-for i in range(qt_alunos):
-    print(f"Aluno {i+1}: ")
-soma = 0
-for j in range(qt_nota):
-    nota = float(input(f"Digite a nota {j+1}: "))
-    soma += nota
-media = soma / qt_nota
-print(f"A média do aluno {i+1} é: {media:.2f}")
-if media >= 7:
-    print("Aprovado")
-else:
-    if media >= 5:
-        print("Recuperação")
-    else:
-        print("Reprovado")
+idade = int(input("qual sua idade: "))
+autorizacao = input("tem autorização? sim ou não: ")
+permissao = "NAO"
 
+if idade >= 16:
+    permissao = "SIM"
+elif autorizacao == "sim":
+    permissao = "SIM"
+
+if permissao == "SIM":
+    if idade < 16:
+        print("Entrada permitida na festa, com autorização")
+    else:
+        print("Entrada permitida na festa.")
+else:
+    print("Entrada negada!")
