279e73a29bf5457ca86f6aadf69117ccc5cec1ef
luizcalos-124 <luizcarlos.tec1@gmail.com>
2026-05-29 08:22:58 -0300

    lc

--- Luiz carlos/atividade2.py
+++ Luiz carlos/atividade2.py
@@ -1,26 +1,11 @@
-alunos = int(input("Insira a quantidade de alunos: "))
-notas = int(input("Insira a quantidade de notas: "))
+idade = int(input("Digite a idade do aluno: "))
+autorizacao = input("Possui autorização dos responsáveis? (sim ou não): ")
 
-for i in range(alunos):
-    soma = 0
-    nome = input("\nDigite o nome do aluno: ")
-
-    for j in range(notas):
-        nota = -1 
-        for tentativa in range(100):
-            nota = float(input(f"Insira a nota {j+1} do aluno {nome}: "))
-            if 0 <= nota <= 10:
-                break
-            elif nota < 0 or nota > 10:
-                print("Nota inválida! Digite um valor entre 0 e 10.")
-        soma += nota
-
-    media = soma / notas
-    print(f"Média do aluno {nome}: {media:.2f}")
-
-    if media >= 6:
-        print("Aluno aprovado.\n")
-    elif media >= 4:
-        print("Recuperação.\n")
-    elif media < 4:
-        print("Reprovado.\n")
+if idade >= 18:
+    print("Entrada permitida.")
+elif idade >= 16 and autorizacao == "sim":
+    print("Entrada permitida.")
+elif idade < 16 and autorizacao == "sim":
+    print("Entrada permitida apenas com responsável.")
+else:
+    print("Entrada não premitida no evento.")
