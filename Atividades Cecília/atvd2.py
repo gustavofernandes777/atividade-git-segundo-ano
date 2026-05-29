74de5ae2ccf00c547663d71bec7fd3ea15ea60ee
Cecilianaiane <cecilia.naiane.tec2025@gmail.com>
2026-05-29 07:52:17 -0300

    Create atvd2.py

---
+++ Atividades Cecília/atvd2.py
@@ -0,0 +1,21 @@
+alunos = int(input("Quantos alunos deseja cadastrar? "))
+notas = int(input("Quantas notas cada aluno terá? "))
+
+for i in range(alunos):
+    print(f"/nAluno {i+1}")
+    nome = input("Nome do aluno: ")
+    
+    soma = 0
+    for j in range(notas):
+        nota = float(input(f"Digite a nota {j+1}: "))
+        soma += nota
+    
+    media = soma /notas
+    print(f"Média de {nome}: {media:.2f}")
+    
+    if media >= 6:
+        print("Situação: Aprovado")
+    elif media < 4:
+        print("Situação: Reprovado")
+    else:
+        print("Situação: Recuperação")
