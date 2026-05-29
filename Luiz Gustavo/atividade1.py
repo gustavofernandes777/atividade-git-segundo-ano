d5e3356e427515a1cbea4646e35435e5c811bbb0
harley <luiz.gustavo.tec2025@gmail.com>
2026-05-29 08:14:10 -0300

    Update atividade1.py

--- Luiz Gustavo/atividade1.py
+++ Luiz Gustavo/atividade1.py
@@ -3,16 +3,28 @@ notas = int(input("Insira a quantidade de notas: "))
 
 for i in range(alunos):
     soma = 0
-    print("Digite o nome do aluno: ")
-    nome = input()
-    for nota in range(notas):
-        print("Insira a nota do aluno: ")
-        nota = float(input())
-        soma+=nota
-    media = soma/notas                       
-    if media>=6:
-        print("aluno aprovado.")
-    elif media <6 and media>=4:
+    
+    nome = input("Digite o nome do aluno: ")
+
+    for j in range(notas):
+
+        while True:
+            nota = float(input("Insira a nota do aluno: "))
+
+            if 0 <= nota <= 10:
+                break
+            else:
+                print("Nota inválida! Digite novamente.")
+
+        soma += nota
+
+    media = soma / notas
+
+    print(f"Média de {nome}: {media:.2f}")
+
+    if media >= 6:
+        print("Aluno aprovado.")
+    elif media >= 4:
         print("Recuperação.")
-    elif media<4:
+    else:
         print("Reprovado.")
