4288c1db350c2a79483a7ce1ed1c6c61953c94f3
Yasminroch4 <yasmin.tec2025@gmail.com>
2026-05-29 23:57:19 -0300

    Atividade corrigida 1 - Yasmin

--- Yasmin Pereira/Atividade2.py
+++ Yasmin Pereira/Atividade2.py
@@ -1,14 +1,15 @@
 alunos = int(input("Quantas alunos deseja cadastrar? "))
 notas = int(input("Quantas notas para cada aluno deseja cadatrar? "))
 
-
-
-
 for i in range(alunos):
     soma = 0
     nome = input("Insira o nome do aluno: ")
     for nota in range(notas):
-        nota = int(input("Digite a nota: "))
+        nota = float(input("Digite a nota: "))
+
+        while nota < 0 or nota > 10:
+             nota = float(input("Digite a nota: "))
+
         soma+=nota
     media = soma/notas
 
